a76129a7f45eaae0f9ebca5f6d59d528d905ac7e
Murat Tokak <tokakmurat01@gmail.com>
2026-01-24 02:21:50 +0300

    View Component, head component, navbar, sidebar ve admin layout düzenlemeleri

---
+++ InsureYouAI/ViewComponents/AdminLayoutViewComponents/_AdminLayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAINew.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ InsureYouAI/ViewComponents/AdminLayoutViewComponents/_AdminLayoutSwitcherComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAINew.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutSwitcherComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
