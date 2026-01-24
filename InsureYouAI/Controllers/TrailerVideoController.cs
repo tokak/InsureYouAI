d4bad336207fbc883b5ec557e04b96cfdbce030e
Murat Tokak <tokakmurat01@gmail.com>
2026-01-24 15:18:11 +0300

    video crud işlemlerinin tamamlanması

---
+++ InsureYouAI/Controllers/TrailerVideoController.cs
@@ -0,0 +1,63 @@
+﻿using InsureYouAI.Context;
+using InsureYouAI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.Controllers
+{
+    public class TrailerVideoController : Controller
+    {
+        private readonly InsureContext _context;
+        public TrailerVideoController(InsureContext context)
+        {
+            _context = context;
+        }
+        public IActionResult TrailerVideoList()
+        {
+            ViewBag.ControllerName = "Sigorta Tanıtım Videosu";
+            ViewBag.PageName = "Ana Sayfa Sigorta Tanıtım Videosu";
+            var values = _context.TrailerVideos.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTrailerVideo()
+        {
+            ViewBag.ControllerName = "Sigorta Tanıtım Videosu";
+            ViewBag.PageName = "Yeni Sigorta Tanıtım Videosu Girişi";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTrailerVideo(TrailerVideo trailerVideo)
+        {
+            _context.TrailerVideos.Add(trailerVideo);
+            _context.SaveChanges();
+            return RedirectToAction("TrailerVideoList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateTrailerVideo(int id)
+        {
+            ViewBag.ControllerName = "Sigorta Tanıtım Videosu";
+            ViewBag.PageName = "Sigorta Tanıtım Videosu Düzenleme Sayfası";
+            var value = _context.TrailerVideos.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateTrailerVideo(TrailerVideo trailerVideo)
+        {
+            _context.TrailerVideos.Update(trailerVideo);
+            _context.SaveChanges();
+            return RedirectToAction("TrailerVideoList");
+        }
+
+        public IActionResult DeleteTrailerVideo(int id)
+        {
+            var value = _context.TrailerVideos.Find(id);
+            _context.TrailerVideos.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("TrailerVideoList");
+        }
+    }
+}
